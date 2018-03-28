bc8b74b26112b379a5a1a04cd8b96a0169e3e9d1
Abdul Rehman <a_rehmanhameed@hotmail.com>
2018-03-28 22:43:42 +0300

    Editted the file with code comments

--- app/src/main/java/com/arehmanhameed/imagecompression/CompressGalleryImage.java
+++ app/src/main/java/com/arehmanhameed/imagecompression/CompressGalleryImage.java
@@ -17,40 +17,76 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class CompressGalleryImage extends AppCompatActivity {
+    //Declaring objects
     private int GET_IMAGE = 1234;
     private Uri URI;
     private InputStream inputStream;
     private Bitmap originalBitmap;
     private ImageView originalImageView, scaledImageView;
     private RelativeLayout progressLayout;
-
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compress_gallery_image);
+
+        //Set title to activity
         this.setTitle("Compress Gallery Image");
+
+        //Get support action bar to enable back arrow
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        originalImageView = (ImageView) findViewById(R.id.original_image);
-        scaledImageView = (ImageView) findViewById(R.id.scaled_image);
-        progressLayout = (RelativeLayout) findViewById(R.id.progress_layout);
+
+        //Initialization of objects
+        originalImageView = findViewById(R.id.original_image);
+        scaledImageView = findViewById(R.id.scaled_image);
+        progressLayout = findViewById(R.id.progress_layout);
     }
 
+    /**
+     * Responsible for all click events on activity_compress_gallery_image views
+     * @param view
+     */
     public void onClickGetGalleryImage(View view) {
-        Intent intent = new Intent();
+        //Initialize camera intent
+        intent = new Intent();
+
+        //Set action to intent to get content
         intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        //Set content type to be "Image"
         intent.setType("image/*");
+
+        //Start activity for result
         startActivityForResult(Intent.createChooser(intent, "Select Source"), GET_IMAGE);
+
+        //Show progress bar
         progressLayout.setVisibility(View.VISIBLE);
     }
 
+
+    /**
+     * Responsible for actions after gallery intent is finished
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Check if the requestCode is equal to GET_IMAGE = 1234,
+        //and resultCode is equal to RESULT_OK (Which means we have a result as an image)
         if (requestCode == GET_IMAGE && resultCode == RESULT_OK) {
+
+            //Get image path
             URI = data.getData();
             try {
+                //Set input Stream from URI
                 inputStream = getContentResolver().openInputStream(URI);
+
+                //Get original image as bitmap from input stream
                 originalBitmap = BitmapFactory.decodeStream(inputStream);
+
+                //Wait for 1 sec
                 new CountDownTimer(1000, 1000) {
 
                     @Override
@@ -60,8 +96,13 @@ public class CompressGalleryImage extends AppCompatActivity {
 
                     @Override
                     public void onFinish() {
+                        //Set original image to original image view
                         originalImageView.setImageBitmap(originalBitmap);
+
+                        //Get and Set scaled image to scaled image view
                         scaledImageView.setImageBitmap(Compressing.letsCompress(getApplicationContext(), URI));
+
+                        //Hide the progress bar
                         progressLayout.setVisibility(View.GONE);
                     }
                 }.start();
